f1d13b6979fcd774034a06876c0183eaf253efb3
Fulela Makasi <fulela.makasi@gmail.com>
2025-04-12 07:36:58 +0200

    fixed navigation for all pages

--- resources/views/interests/index.blade.php
+++ resources/views/interests/index.blade.php
@@ -10,11 +10,14 @@
 
 <nav class="navbar navbar-inverse">
     <div class="navbar-header">
-        <a class="navbar-brand" href="{{ URL::to('interests') }}">interest Alert</a>
+        <a class="navbar-brand" href="{{ URL::to('dashboard') }}">Propay</a>
     </div>
     <ul class="nav navbar-nav">
-        <li><a href="{{ URL::to('interests') }}">View All interests</a></li>
-        <li><a href="{{ URL::to('interests/create') }}">Create an interest</a>
+        <li><a href="{{ URL::to('interests') }}">View All Interests</a></li>
+        <li><a href="{{ URL::to('interests/create') }}">Create an Interest</a>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+        <li><a href="{{ route('logout') }}" class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] text-[#1b1b18] border border-transparent hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm text-sm leading-normal"> Logout</a></li>
     </ul>
 </nav>
 
